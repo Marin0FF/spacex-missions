54949347f5bbdb587529611a24b102713deb9371
Marin0FF <hmarinov1977@gmail.com>
2021-06-02 19:55:49 +0100

    initial setup

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,12 @@
 export default function Hero() {
     return (
-        <p>Test Hero</p>
+        <section className="hero has-background-black">
+            <div className="hero-body is-flex is-align-items-center">
+                <div className="container has-text-centered">
+                    <p className="title is-1 has-text-white">History of SpaceX <br /> missions</p>
+                    <button className="button is-white is-medium is-outlined">Launch</button>
+                </div>
+            </div>
+        </section>
     )
 };
